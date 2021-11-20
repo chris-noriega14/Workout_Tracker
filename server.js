cc8660d238b02b6efd6fad8fcea469c0b7d586bb
chris-noriega14 <chris.noriega14@hotmail.com>
2021-11-19 18:08:06 -0600

    Updated MongoDB setup

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(logger("dev"));
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/workout", {
+mongoose.connect(process.env.MONGODB || "mongodb://localhost:27017/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
